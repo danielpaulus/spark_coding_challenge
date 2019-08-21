a9d05b2a00d4a0a80c802acf7ae9423d68db46f7
Daniel Paulus <daniel.paulus@saucelabs.com>
2019-08-21 21:44:14 +0200

    add question type distinction

--- survey-app/src/Question.js
+++ survey-app/src/Question.js
@@ -3,7 +3,17 @@ import React, { Component } from 'react'
 class Question extends Component{
 
 render(){
-    return <h3>Question </h3>
+    const title = <h3>{this.props.details.question} </h3>;
+    var questionType = '';
+    switch (this.props.details.question_type.type){
+    case 'single_choice':
+         questionType = <p>single_choice</p>;
+         break;
+    case 'single_choice_conditional':
+    questionType = <p>single_choice_conditional</p>;
+             break;
+    }
+    return  <div> {title} {questionType} </div>
 
 }
 }
