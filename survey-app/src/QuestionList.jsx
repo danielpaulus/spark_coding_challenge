e47af35801152a2aaf8e81898c3c7f36a80879c4
Daniel Paulus <daniel.paulus@saucelabs.com>
2019-08-21 16:47:22 +0200

    remove useless log

--- survey-app/src/QuestionList.jsx
+++ survey-app/src/QuestionList.jsx
@@ -4,7 +4,6 @@ import Question from './Question'
 class QuestionList extends Component{
 
 render(){
-console.log(this.props.questions.filter(question => question.category === this.props.category));
     return  <div><h2>Category {this.props.category}</h2> <div>{this.props.questions.filter(question => question.category === this.props.category).map(question => {
                 return <Question details={question}/>
             })}</div></div>;
