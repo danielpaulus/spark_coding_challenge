b882a6a334c5babdc49f6ff145cdecd11769fdbe
Daniel Paulus <daniel.paulus@saucelabs.com>
2019-08-21 16:46:43 +0200

    added component for displaying questions

---
+++ survey-app/src/QuestionList.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import Question from './Question'
+
+class QuestionList extends Component{
+
+render(){
+console.log(this.props.questions.filter(question => question.category === this.props.category));
+    //<h2>Category {this.props.category}</h2>
+    return  <div><h2>Category {this.props.category}</h2> <div>{this.props.questions.filter(question => question.category === this.props.category).map(question => {
+                return <Question details={question}/>
+            })}</div></div>;
+}
+}
+
+export default QuestionList
