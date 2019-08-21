201a4d1ca17bcfad2d7f29a793deef3b51914ab8
Daniel Paulus <daniel.paulus@saucelabs.com>
2019-08-21 16:47:08 +0200

    remove comment

--- survey-app/src/QuestionList.jsx
+++ survey-app/src/QuestionList.jsx
@@ -5,7 +5,6 @@ class QuestionList extends Component{
 
 render(){
 console.log(this.props.questions.filter(question => question.category === this.props.category));
-    //<h2>Category {this.props.category}</h2>
     return  <div><h2>Category {this.props.category}</h2> <div>{this.props.questions.filter(question => question.category === this.props.category).map(question => {
                 return <Question details={question}/>
             })}</div></div>;
