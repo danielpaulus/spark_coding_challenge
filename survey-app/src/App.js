e7ab3d4069088dd950091209a09cee1407f8eff4
Daniel Paulus <daniel.paulus@saucelabs.com>
2019-08-22 20:42:05 +0200

    fix rendering before data arrived

--- survey-app/src/App.js
+++ survey-app/src/App.js
@@ -13,14 +13,14 @@ class App extends Component {
         fetch('/api/surveys')
         .then(res => res.json())
         .then((data) => {
-          this.setState({ survey: data[0] })
+          this.setState({ survey: data[0] });
         })
         .catch(console.log)
 
         fetch('/api/categories')
                 .then(res => res.json())
                 .then((data) => {
-                  this.setState({ categories: data })
+                  this.setState({ categories: data });
                 })
                 .catch(console.log)
       }
@@ -29,8 +29,12 @@ render(){
   return (
     <div className="App">
       <header className="App-header">
-            <h1></h1>
-            <Survey categories={this.state.categories} survey={this.state.survey}/>
+      {
+                this.state.categories.length>0 && Object.entries(this.state.survey).length>0
+                          ? <Survey categories={this.state.categories} survey={this.state.survey}/>
+                          : null
+            }
+
       </header>
     </div>
   );}
