ceaf7eed19cfd3f1369e2212a692def9761cab4e
Daniel Paulus <daniel.paulus@saucelabs.com>
2019-08-22 11:04:00 +0200

    add component for category

--- survey-app/src/App.js
+++ survey-app/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Survey from './Survey';
 const apiUrl = 'http://localhost:4000';
--- survey-app/src/QuestionList.jsx
+++ survey-app/src/QuestionList.jsx
@@ -1,11 +1,21 @@
 import React, {Component} from 'react';
 import Question from './Question';
+import Category from './Category';
 
 class QuestionList extends Component {
   render() {
-    return <div><h2>Category {this.props.category}</h2> <div>{this.props.questions.filter((question) => question.category === this.props.category).map( (question, index) => {
-      return <Question questionIndex={index} details={question}/>;
-    })}</div></div>;
+    return <div>
+            <Category title={this.props.category}/>
+            <div>
+                {
+                this.props.questions.filter((question) => question.category === this.props.category)
+                                     .map( (question, index) => {
+                                            return <Question questionIndex={index} details={question}/>;
+                                                                }
+                                         )
+                 }
+             </div>
+            </div>;
   }
 }
 
