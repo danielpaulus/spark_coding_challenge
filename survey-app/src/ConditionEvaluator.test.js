3ff8cf4c5decf52e65bd50a849470bf206d5eb1e
Daniel Paulus <daniel.paulus@saucelabs.com>
2019-08-23 07:43:07 +0200

    add chai for assertions, add a simple testcase here

--- survey-app/src/ConditionEvaluator.test.js
+++ survey-app/src/ConditionEvaluator.test.js
@@ -1,5 +1,7 @@
 import {evaluateCondition} from './ConditionEvaluator';
+var assert = require('chai').assert
 
-it('checks', () => {
-
+it('returns false for broken input', () => {
+    const result = evaluateCondition("", 1);
+    assert.equal(false, result);
 });
