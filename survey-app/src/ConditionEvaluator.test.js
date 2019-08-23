233eda68595b911f3ae00a6663482969949e2529
Daniel Paulus <daniel.paulus@saucelabs.com>
2019-08-23 08:40:46 +0200

    add some margin, run lint

--- survey-app/src/ConditionEvaluator.test.js
+++ survey-app/src/ConditionEvaluator.test.js
@@ -1,7 +1,7 @@
 import {evaluateCondition} from './ConditionEvaluator';
-var assert = require('chai').assert
+const assert = require('chai').assert;
 
 it('returns false for broken input', () => {
-    const result = evaluateCondition("", 1);
-    assert.equal(false, result);
+  const result = evaluateCondition('', 1);
+  assert.equal(false, result);
 });
